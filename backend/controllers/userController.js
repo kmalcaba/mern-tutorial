ff220542c685860ff523e2e004b4d866219eeff6
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-20 12:59:32 -0700

    Wrap async functions with async handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,22 +1,27 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
 // @desc  Register new user
 // @route POST /api/users
 // @access  Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   res.json({ message: "Register user" });
-};
+});
 
 // @desc  Authenticate user
 // @route POST /api/users/login
 // @access  Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: "Login user" });
-};
+});
 
 // @desc  Get user data
 // @route GET /api/users/me
 // @access  Public
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "Get user data " });
-};
+});
 
 module.exports = { registerUser, loginUser, getMe };
