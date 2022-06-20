d2417c9420323b44be47508e31a948f4b06a0f2c
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-20 13:29:10 -0700

    Create a generate JWT function for registering and authenticating

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -73,5 +74,10 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "Get user data " });
 });
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET), { expiresIn: "30d" };
+};
 
 module.exports = { registerUser, loginUser, getMe };
