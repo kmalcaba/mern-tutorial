ef3fdc2a68c317fd53737f07b5324da534fdc851
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-14 14:53:04 -0700

    Import state store

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,41 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { reset } from "../features/auth/authSlice";
+import { getGoals } from "../features/goals/goalSlice";
 import GoalForm from "../components/GoalForm";
+import Spinner from "../components/Spinner";
 
 function Dashboard() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.goals
+  );
 
   useEffect(() => {
     if (!user) {
       navigate("/login");
     }
-  }, [user, navigate]);
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, dispatch]);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    dispatch(getGoals());
+  }, [dispatch, isError, message]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
