bec48809512ba95c1787063823a6ac98e1a3d728
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-14 13:58:21 -0700

    Add thunk for getting all goals

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -3,19 +3,32 @@ import axios from "axios";
 const API_URL = "api/goals";
 
 const createGoal = async (goalData, token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
+  const config = createConfig(token);
 
   const response = await axios.post(API_URL, goalData, config);
 
   return response.data;
 };
 
+const getGoals = async (token) => {
+  const config = createConfig(token);
+
+  const response = await axios.get(API_URL, config);
+
+  return response.data;
+};
+
+const createConfig = (token) => {
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+};
+
 const goalService = {
   createGoal,
+  getGoals,
 };
 
 export default goalService;
