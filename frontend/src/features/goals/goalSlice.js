ecc87eef8bd8e055189a4537c81507ade4632d09
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-14 12:46:44 -0700

    Create goal basic reducers and service file

---
+++ frontend/src/features/goals/goalSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import goalService from "./goalService";
+
+const initialState = {
+  goals: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+export const addGoal = createAsyncThunk("goal/add", async (goal, thunkAPI) => {
+  try {
+    return await goalService.addGoal(goal);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
+export const goalSlice = createSlice({
+  name: "goal",
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {},
+});
+
+export const { reset } = goalSlice.actions;
+export default goalSlice.reducer;
