3a564e27d8b5a14a09fec3680a9402f55f42d8f1
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-14 15:37:42 -0700

    Configure for production build

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const colors = require("colors");
@@ -23,6 +24,19 @@ const userRoutes = require("./routes/userRoutes");
 
 app.use("/api/goals", goalRoutes);
 app.use("/api/users", userRoutes);
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.use(errorHandler);
 
