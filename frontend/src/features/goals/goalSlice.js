4e8c657a32dd65c7997a686da7e722b711a51a1e
Kirsten Malcaba <kmalcaba@gmail.com>
2022-07-14 13:47:15 -0700

    Configur reducers, thunk functions, etc

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -9,18 +9,24 @@ const initialState = {
   message: "",
 };
 
-export const addGoal = createAsyncThunk("goal/add", async (goal, thunkAPI) => {
-  try {
-    return await goalService.addGoal(goal);
-  } catch (error) {
-    const message =
-      (error.response && error.response.data && error.response.data.message) ||
-      error.message ||
-      error.toString();
+export const createGoal = createAsyncThunk(
+  "goals/create",
+  async (goal, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await goalService.createGoal(goal, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
 
-    return thunkAPI.rejectWithValue(message);
+      return thunkAPI.rejectWithValue(message);
+    }
   }
-});
+);
 
 export const goalSlice = createSlice({
   name: "goal",
@@ -28,7 +34,22 @@ export const goalSlice = createSlice({
   reducers: {
     reset: (state) => initialState,
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createGoal.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createGoal.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.goals.push(action.payload);
+      })
+      .addCase(createGoal.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
 });
 
 export const { reset } = goalSlice.actions;
